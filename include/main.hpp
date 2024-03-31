a09db2cc4b712c2901281c04ead4c2c222510f95
Jonas W <hdgamer1404Jonas@gmail.com>
2024-04-01 00:27:20 +0200

    Update for 1.35

--- include/main.hpp
+++ include/main.hpp
@@ -12,4 +12,5 @@
 
 // Define these functions here so that we can easily read configuration and log information from other files
 Configuration& getConfig();
-Logger& getLogger();
+
+constexpr auto logger = Paper::ConstLoggerContext(MOD_ID);
